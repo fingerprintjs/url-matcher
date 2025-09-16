ea5db24b69062d0c57a4c851d44f3ae6dea7bcd2
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-09-16 16:17:31 +0200

    test: run tests using Node.js and JSDom

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,22 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testRegex: '/__tests__/.+test.tsx?$',
-  collectCoverageFrom: ['./src/**/**.{ts,tsx}'],
+  projects: [
+    {
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      testRegex: '/__tests__/.+test.tsx?$',
+      collectCoverageFrom: ['./src/**/**.{ts,tsx}'],
+      setupFilesAfterEnv: ['./__tests__/setup.ts'],
+      displayName: 'Node.js',
+    },
+    {
+      preset: 'ts-jest',
+      testEnvironment: 'jsdom',
+      testRegex: '/__tests__/.+test.tsx?$',
+      collectCoverageFrom: ['./src/**/**.{ts,tsx}'],
+      setupFilesAfterEnv: ['./__tests__/setup.ts'],
+      displayName: 'Browser (JSDom)',
+    },
+  ],
   coverageReporters: ['lcov', 'json-summary', ['text', { file: 'coverage.txt', path: './' }]],
-  setupFilesAfterEnv: ['./__tests__/setup.ts'],
 }
